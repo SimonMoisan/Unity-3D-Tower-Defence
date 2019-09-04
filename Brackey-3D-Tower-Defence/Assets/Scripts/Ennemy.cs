dcc31f1328731383a9576dba0efbff8df1e63b2e
MiisterB <40004939+MiisterB@users.noreply.github.com>
2019-09-04 16:47:28 +0200

    First commit

---
+++ Brackey-3D-Tower-Defence/Assets/Scripts/Ennemy.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ennemy : MonoBehaviour
+{
+    [SerializeField] public float speed;
+    public GameObject explosion;
+
+    private Transform target;
+    private int wavepointIndex = 0;
+
+    void Start()
+    {
+        target = EnnemyPath.points[wavepointIndex];
+    }
+
+    void Update()
+    {
+        Vector3 targetPosition = target.position - transform.position;
+        transform.Translate(targetPosition.normalized * speed * Time.deltaTime, Space.World);
+
+        if(Vector3.Distance(transform.position, target.position) <= 0.2f)
+        {
+            GetNextWaypoint();
+        }
+    }
+
+    void GetNextWaypoint()
+    {
+        if(wavepointIndex >= EnnemyPath.points.Length -1)
+        {
+            Destroy(gameObject);
+        }
+
+        wavepointIndex++;
+        target = EnnemyPath.points[wavepointIndex];
+    }
+}
---
+++ Brackey-3D-Tower-Defence/Assets/Scripts/EnnemyPath.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnnemyPath : MonoBehaviour
+{
+    public static Transform[] points;
+
+    //Get all childrens in an array
+    void Awake()
+    {
+        points = new Transform[transform.childCount];
+        for(int i = 0;i < points.Length; i++)
+        {
+            points[i] = transform.GetChild(0);
+        }
+    }
+}
